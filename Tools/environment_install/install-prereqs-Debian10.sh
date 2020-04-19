3e86f368048c97644543ee79b778c430a21f817f
Azkarel <antonio@DarthVader.azkarel.es>
2020-04-19 20:30:55 +0200

    Bash script for compiling environment preparation under Debian10

---
+++ Tools/environment_install/install-prereqs-Debian10.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+echo "---------- $0 start ----------"
+set -e
+set -x
+
+if [ $EUID == 0 ]; then
+    echo "Please do not run this script as root; don't sudo it!"
+    exit 1
+fi
+
+# define directory where GNU Tools for ARM Embedded Processors will be installed
+
+OPT="/opt"
+
+# select SFML packages version (Simple and Fast Multimedia Library)
+
+RELEASE_CODENAME=$(lsb_release -c -s)
+if [ ${RELEASE_CODENAME} == 'jessie' ]; then
+    SITLFML_VERSION="2"
+    SITLCFML_VERSION="2"
+elif [ ${RELEASE_CODENAME} == 'stretch' ]; then
+    SITLFML_VERSION="2.3"
+    SITLCFML_VERSION="2.3"
+elif [ ${RELEASE_CODENAME} == 'buster' ]; then
+    SITLFML_VERSION="2.5"
+    SITLCFML_VERSION="2.5"
+else
+    SITLFML_VERSION="2.5"
+    SITLCFML_VERSION="2.5"
+fi
+
+# base general compile packages, realpath will be included below in the script
+
+BASE_PKGS="build-essential ccache g++ gawk git make wget"
+
+# add some Python packages required for commonly-used MAVProxy modules and hex file generation:
+
+PYTHON_PKGS="future lxml pymavlink MAVProxy pexpect"
+PYTHON_PKGS="$PYTHON_PKGS pygame intelhex"
+
+# base packages needed to compile ardupilot
+
+PX4_PKGS="python-argparse openocd flex bison libncurses5-dev \
+          autoconf texinfo libftdi-dev zlib1g-dev \
+          zip genromfs python-empy cmake cmake-data"
+
+# packages required to compile for linux devices (ARM V5 and following)        
+
+# ARM_LINUX_PKGS="g++-arm-linux-gnueabihf"
+# ARM_LINUX_PKGS+=" pkg-config-arm-linux-gnueabihf" This is a UBUNTU package and will be intalled at the end
+
+# STIL packages (Software In The Loop) python-wxgtk packages are added below in the script
+
+SITL_PKGS="libtool libxml2-dev libxslt1-dev python-dev python-pip python-setuptools python-matplotlib python-serial python-scipy python-opencv python-numpy python-pyparsing xterm lcov gcovr"
+
+# add some packages for use SFML (Simple and Fast Multimedia Library) and YAML 
+
+SITL_PKGS="$SITL_PKGS libcsfml-dev libcsfml-audio${SITLCFML_VERSION} libcsfml-dev libcsfml-graphics${SITLCFML_VERSION} libcsfml-network${SITLCFML_VERSION} libcsfml-system${SITLCFML_VERSION} libcsfml-window${SITLCFML_VERSION} libsfml-audio${SITLFML_VERSION} libsfml-dev libsfml-graphics${SITLFML_VERSION} libsfml-network${SITLFML_VERSION} libsfml-system${SITLFML_VERSION} libsfml-window${SITLFML_VERSION} python-yaml python3-yaml"
+
+ASSUME_YES=false
+QUIET=false
+
+# add libc6-i386 library in case of AMD64 computer arquitecture
+
+MACHINE_TYPE=$(uname -m)
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    PX4_PKGS+=" libc6-i386"
+else
+  echo "no extra pkgs for i386"
+fi
+
+# GNU Tools for ARM Embedded Processors
+# (see https://launchpad.net/gcc-arm-embedded/)
+
+ARM_ROOT="gcc-arm-none-eabi-6-2017-q2-update"
+ARM_TARBALL="$ARM_ROOT-linux.tar.bz2"
+ARM_TARBALL_URL="https://firmware.ardupilot.org/Tools/STM32-tools/$ARM_TARBALL"
+
+# Ardupilot Tools directory
+
+ARDUPILOT_TOOLS="Tools/autotest"
+
+# make user member of group dialout
+
+sudo usermod -a -G dialout $USER
+
+# prepare apt-get options
+
+function maybe_prompt_user() {
+    if $ASSUME_YES; then
+        return 0
+    else
+        read -p "$1"
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            return 0
+        else
+            return 1
+        fi
+    fi
+}
+
+OPTIND=1  # Reset in case getopts has been used previously in the shell.
+while getopts "y" opt; do
+    case "$opt" in
+        \?)
+            exit 1
+            ;;
+        y)  ASSUME_YES=true
+            ;;
+        q)  QUIET=true
+            ;;
+    esac
+done
+
+APT_GET="sudo apt-get"
+if $ASSUME_YES; then
+    APT_GET="$APT_GET --assume-yes"
+fi
+if $QUIET; then
+    APT_GET="$APT_GET -qq"
+fi
+
+$APT_GET update
+
+# install Linux Standard Base version reporting utility
+
+if ! dpkg-query -l "lsb-release"; then
+    $APT_GET install lsb-release
+fi
+
+# uninstall modemmanager in case it was installed
+
+if dpkg-query -l "modemmanager"; then
+    $APT_GET remove modemmanager
+fi
+
+# add python-wxgtk to SITL_PKGS
+
+if apt-cache search python-wxgtk3.0 | grep wx; then
+    SITL_PKGS+=" python-wxgtk3.0 libtool-bin"
+else
+    # we only support back to trusty:
+    SITL_PKGS+=" python-wxgtk2.8"
+fi
+
+# add realpath to BASE_PKGS
+
+RP=$(apt-cache search -n '^realpath$')
+if [ -n "$RP" ]; then
+    BASE_PKGS+=" realpath"
+fi
+
+# main package installation
+
+$APT_GET install $BASE_PKGS $SITL_PKGS $PX4_PKGS $ARM_LINUX_PKGS
+pip2 -q install --user -U $PYTHON_PKGS
+
+# install GNU Tools for ARM Embedded Processors
+
+if [ ! -d $OPT/$ARM_ROOT ]; then
+    (
+        cd $OPT;
+        sudo wget $ARM_TARBALL_URL;
+        sudo tar xjf ${ARM_TARBALL};
+        sudo rm ${ARM_TARBALL};
+    )
+fi
+
+# include GNU Tools for ARM Embedded Processors and Ardupilot tools directories to PATH
+
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+ARDUPILOT_ROOT=$(realpath "$SCRIPT_DIR/../../")
+
+exportline="export PATH=$OPT/$ARM_ROOT/bin:\$PATH";
+grep -Fxq "$exportline" ~/.profile 2>/dev/null || {
+    if maybe_prompt_user "Add $OPT/$ARM_ROOT/bin to your PATH [N/y]?" ; then
+        echo $exportline >> ~/.profile
+        eval $exportline
+    else
+        echo "Skipping adding $OPT/$ARM_ROOT/bin to PATH."
+    fi
+}
+
+exportline2="export PATH=$ARDUPILOT_ROOT/$ARDUPILOT_TOOLS:\$PATH";
+grep -Fxq "$exportline2" ~/.profile 2>/dev/null || {
+    if maybe_prompt_user "Add $ARDUPILOT_ROOT/$ARDUPILOT_TOOLS to your PATH [N/y]?" ; then
+        echo $exportline2 >> ~/.profile
+        eval $exportline2
+    else
+        echo "Skipping adding $ARDUPILOT_ROOT/$ARDUPILOT_TOOLS to PATH."
+    fi
+}
+
+#
+
+apt-cache search arm-none-eabi
+
+# actualize ardupilot modules
+
+(
+ cd $ARDUPILOT_ROOT
+ git submodule update --init --recursive
+)
+
+echo "Done. Please log out and log in again."
+
+echo "---------- $0 end ----------"
